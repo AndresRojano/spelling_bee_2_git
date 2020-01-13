9b8109d83ce2be4359adcdafde3045d5b9e8e816
Andres Rojano <arojanomtz@hotmail.com>
2020-01-13 00:48:59 -0600

    0

---
+++ js/acciones.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function(e) {
+	$('#boton1').on('click',function(){
+		alert("1");
+	});//boton1
+	$('#boton2').on('click',function(){
+		alert("2");
+    });//boton2
+	$('#boton3').on('click',function(){
+		alert("3");
+    });//boton3
+}); //ready
